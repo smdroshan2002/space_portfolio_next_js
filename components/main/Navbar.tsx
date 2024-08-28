8def77e6ed756a73f99805d2cff02201a72c36eb
Shaik Mohammed Roshan <148673311+smdroshan4@users.noreply.github.com>
2024-08-28 10:57:10 +0530

    Update Navbar.tsx

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -1,14 +1,31 @@
+"use client";
+
 import { Socials } from "@/constants";
-import Image from 'next/image';
-import React from 'react';
+import Image from "next/image";
+import React from "react";
 
 const Navbar = () => {
+  const handleClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    console.log("Button clicked!"); // Debugging line
+    event.preventDefault();
+    const targetId = event.currentTarget.getAttribute("href");
+
+    if (targetId && targetId.startsWith("#")) {
+      const targetElement = document.querySelector(targetId);
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  };
+
   return (
-    <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
-      <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
+    <div className="w-full h-[65px] fixed top-0 shadow-lg bg-[#03001417] z-[999] px-10">
+      <div className="w-full h-full flex items-center justify-between m-auto px-[10px]">
         <a
           href="#about-me"
-          className="h-auto w-auto flex flex-row items-center"
+          onClick={handleClick}
+          className="flex items-center"
+          style={{ zIndex: 10 }}
         >
           <Image
             src="/boy.png"
@@ -21,33 +38,34 @@ const Navbar = () => {
             Mohammed Roshan S
           </span>
         </a>
-        <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
-          <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
-            <a href="#about-me" className="cursor-pointer">
+        <div className="w-[500px] flex items-center justify-between md:mr-20">
+          <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] px-[20px] py-[10px] rounded-full text-gray-200">
+            <a href="#about-me" onClick={handleClick} className="cursor-pointer">
               About Me
             </a>
-            <a href="#skills" className="cursor-pointer">
+            <a href="#skills" onClick={handleClick} className="cursor-pointer">
               Skills
             </a>
-            <a href="#projects" className="cursor-pointer">
+            <a href="#projects" onClick={handleClick} className="cursor-pointer">
               Projects
             </a>
           </div>
         </div>
-        <div className="flex flex-row gap-5">
+        <div className="flex gap-5">
           {Socials.map((social) => (
             <a
               href={social.link}
               key={social.name}
               target="_blank"
               rel="noopener noreferrer"
+              className="cursor-pointer"
+              style={{ zIndex: 10 }}
             >
               <Image
                 src={social.src}
                 alt={social.name}
                 width={24}
                 height={24}
-                className="cursor-pointer"
               />
             </a>
           ))}
@@ -55,6 +73,6 @@ const Navbar = () => {
       </div>
     </div>
   );
-}
+};
 
 export default Navbar;
