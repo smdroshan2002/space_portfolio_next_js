dbad16da8ff5d164d3d2bc519af09381b5338a26
Shaik Mohammed Roshan <148673311+smdroshan2002@users.noreply.github.com>
2025-05-19 13:07:21 +0530

    Update Hero.tsx

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
         autoPlay
         muted
         loop
-        className="rotate-180 absolute top-[-140px] left-0 h-full w-full z-[1] object-cover">
+        className="absolute top-0 left-0 h-full w-full z-[1] object-cover scale-[1.2] brightness-[0.7]">
         <source src="/rotated_sun_video.mp4" type="video/mp4" />
       </video>
       <HeroContent className="relative z-[1]" />
