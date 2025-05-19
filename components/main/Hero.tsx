13868c7a1927d14a2142c0e3d93c67e2acbb01c8
Shaik Mohammed Roshan <148673311+smdroshan2002@users.noreply.github.com>
2025-05-19 13:02:21 +0530

    Update Hero.tsx

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
         muted
         loop
         className="rotate-180 absolute top-[-140px] left-0 h-full w-full z-[1] object-cover">
-        <source src="/blackhole.webm" type="video/webm" />
+        <source src="/rotated_sun_video.mp4" type="video/mp4" />
       </video>
       <HeroContent className="relative z-[1]" />
     </div>
